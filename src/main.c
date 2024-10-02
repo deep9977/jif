4e217f28929afdf071920850b5a91a43f13cd7c8
deep <166912166+deep9977@users.noreply.github.com>
2024-10-02 00:03:53 -0400

    added precalculated conditional jumps in intermediate representation for loops

--- src/main.c
+++ src/main.c
@@ -26,6 +26,12 @@ typedef struct {
 }
 INST;
 
+//only used in tansform()
+typedef struct{
+    size_t stack[MAX_STACK_CAPACITY];
+    size_t sp;
+}Stack;
+
 void usage(char* s) {
     fprintf(stderr, "Usage: %s <prog.bf>", s);
     exit(EXIT_FAILURE);
@@ -80,22 +86,23 @@ char get_char(char* prog) {
 
 void transform(char* prog, INST inst[MAX_INST_CAPACITY]) {
     char c = get_char(prog++), c1;
-    int i = 0;
+    int i = 0, n;
+    Stack s1 ={0};
 
     while (c) { 
         if (i >= MAX_INST_CAPACITY) {
-            fprintf(stderr, "Instruction capacity exceeded\n");
+            fprintf(stderr, "Transform: Instruction capacity exceeded\n");
             exit(EXIT_FAILURE);
         }
 
         switch (c) {
-            case '+':
+            case INST_INC:
             case INST_DEC:
             case INST_MOVL:
             case INST_MOVR:
             case INST_OUT:
-            case INST_IN:
-                int n = 1;
+            case INST_IN: {
+                n = 1;
                 char c1;
 
                 while ((c1 = get_char(prog++)) == c) {
@@ -107,16 +114,48 @@ void transform(char* prog, INST inst[MAX_INST_CAPACITY]) {
                 i++;
                 c = c1; 
                 break;
-            
+            }
 
-                
+            case INST_JMP_IF_ZERO: {
+                if(s1.sp >= MAX_STACK_CAPACITY ){
+                    fprintf(stderr, "Transform: Stack overflow");
+                    exit(EXIT_FAILURE);
+                }
+
+                s1.stack[s1.sp++] = i; 
+
+                inst[i].opcode = c;
+                inst[i].operand = 0;
+                i++;
+
+                c = get_char(prog++);
+                break;
+            }
+
+            case INST_JMP_IF_NONZERO: { 
+                if(s1.sp <= 0){
+                    fprintf(stderr, "Transform: Stack underflow, unmatched brackets");
+                    exit(EXIT_FAILURE);
+                }
+
+                n = s1.stack[--s1.sp];
+
+                inst[i].opcode = c;
+                inst[i].operand = n;
+                i++;
+                inst[n].operand = i;
+
+
+                c = get_char(prog++);
+                break;
+            }
+
             default:
-                printf("\ntodo");
-                exit(0);
+                fprintf(stderr, "Transform: Unexpected character");
+                exit(EXIT_FAILURE);
         }
     }
 }
-
 
 int main(int argc, char* argv[]) {
     printf("Starting program...\n");
